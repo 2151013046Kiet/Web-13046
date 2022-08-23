615fb6e9e2af65dffea108de9864eaa658835f62
2151013046Kiet <103587744+2151013046Kiet@users.noreply.github.com>
2022-08-23 15:52:07 +0700

    Add files via upload

---
+++ Web-13046/JS/js03_1.js
@@ -0,0 +1,38 @@
+function PTB2(){
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var c = parseInt(document.getElementById("c").value);
+    var kq, x;
+    if(a == 0)
+        if(b == 0)
+            if(c == 0)
+            kq = "PT có vô số nghiệm";
+            else
+            kq = "PT vô nghiệm";
+        else//b!=0;
+        {
+            x = -c/b;
+            kq = x.toFixed(2);
+        }
+    else{//a!=0
+        var delta = b*b - 4*a*c;
+        if(delta < 0)
+            kq = "Phương trình vô nghiệm";
+        else if (delta == 0){
+            x = -b/(2*a);
+            kq = "Phương trình có nghiệm kép là " + x.toFixed(2); 
+        }
+        else {//delta > 0
+             var x1 = (-b + Math.sqrt(delta)) / (2*a);
+             var x2 = (-b - Math.sqrt(delta)) / (2*a);
+             kq = "Phương trình có hai nghiệm phân biệt: <br>x1 = " + x1.toFixed(2) + "<br>x2 = " + x2.toFixed(2); 
+        }
+    }
+    document.getElementById("ketqua").innerHTML = kq;
+}
+function xoa(){
+    document.getElementById("a").value = "";
+    document.getElementById("b").value = "";
+    document.getElementById("c").value = "";
+    document.getElementById("ketqua").value = "";
+} 
